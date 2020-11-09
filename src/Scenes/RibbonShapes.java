b406de86f898fe128a4f6614301e168fe79654c1
gabrielyin <70323043+gabrielyin@users.noreply.github.com>
2020-11-09 11:48:50 -0300

    09/11/2020

--- src/Scenes/RibbonShapes.java
+++ src/Scenes/RibbonShapes.java
@@ -21,6 +21,7 @@ import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -34,11 +35,14 @@ public class RibbonShapes {
     Image IMG;
     BackgroundImage BCIMG;
     Button APRES,AVANT;
+    HBox[] BOXSEAT;
     HBox BOXOPTIONS,MINIBOX;
+    VBox BOXES;
     ScrollPane SCROLL;
+    ImageView INTER;
     ImageView[] V1IMG;
     Integer[] ACTION;
-    Integer MAKE;
+    Integer MAKE,BINTER;
     
     public RibbonShapes(){
         BPANE = new BorderPane();
@@ -51,8 +55,36 @@ public class RibbonShapes {
             BackgroundSize.DEFAULT);
         
         APRES = new Button(">");
+        APRES.setOnAction((f) -> {
+            MINIBOX.getChildren().clear();
+            INTER = V1IMG[4];
+            BINTER = ACTION[4];
+            for (int i = 4; i > 0; i--) {
+                V1IMG[i] = V1IMG[i-1];
+                ACTION[i] = ACTION[i-1];
+            }
+            V1IMG[0]=INTER;
+            ACTION[0]=BINTER;
+            for (int i = 0; i < 5; i++) {
+                MINIBOX.getChildren().addAll(V1IMG[i]);
+            }
+        });
         
         AVANT = new Button("<");
+        AVANT.setOnAction((e) -> {
+            MINIBOX.getChildren().clear();
+            INTER = V1IMG[0];
+            BINTER = ACTION[0];
+            for (int i = 0; i < 4; i++) {
+                V1IMG[i] = V1IMG[i+1];
+                ACTION[i] = ACTION[i+1];
+            }
+            V1IMG[4] = INTER;
+            ACTION[4] = BINTER;
+            for (int i = 0; i < 5; i++) {
+                MINIBOX.getChildren().addAll(V1IMG[i]);
+            }
+        });
         
         MINIBOX = new HBox();
         V1IMG = new ImageView[5];
@@ -64,7 +96,19 @@ public class RibbonShapes {
         }
         MINIBOX.getChildren().addAll(V1IMG);
         V1IMG[0].setOnMouseClicked((MouseEvent)->{
-//            Action(0);
+            Action(0);
+        });
+        V1IMG[1].setOnMouseClicked((MouseEvent)->{
+            Action(1);
+        });
+        V1IMG[2].setOnMouseClicked((MouseEvent)->{
+            Action(2);
+        });
+        V1IMG[3].setOnMouseClicked((MouseEvent)->{
+            Action(3);
+        });
+        V1IMG[4].setOnMouseClicked((MouseEvent)->{
+            Action(4);
         });
         
         SCROLL = new ScrollPane();
@@ -72,17 +116,59 @@ public class RibbonShapes {
         SCROLL.pannableProperty().set(true);
         
         BOXOPTIONS = new HBox(20);
-        BOXOPTIONS.getChildren().addAll(APRES,AVANT,SCROLL);
+        BOXOPTIONS.getChildren().addAll(AVANT,SCROLL,APRES);
         BOXOPTIONS.setPadding(new Insets(40,40,0,40));
         BOXOPTIONS.setAlignment(Pos.CENTER);
         
+        BOXSEAT = new HBox[4];
+        BOXES = new VBox(2);
+//        for (int i = 0; i < 4; i++) {
+//            BOXSEAT[i] = new HBox(5);
+//            switch(i){
+//                case 0:
+//                    LineSeat(BOXSEAT[i],'A',3);
+//                    break;
+//                case 1:
+//                    LineSeat(BOXSEAT[i],'B',7);
+//                    break;
+//                case 2:
+//                    LineSeat(BOXSEAT[i],'C',7);
+//                    break;
+//                case 3:
+//                    LineSeat(BOXSEAT[i],'D',3);
+//                    break;
+//            }
+//        }
+        
         BPANE.setTop(BOXOPTIONS);
+        BPANE.setCenter(BOXES);
         BPANE.setBackground(new Background(BCIMG));
         ENTRANCE = new Scene(BPANE,450,800,Color.BLACK);
         ENTRANCE.getStylesheets().add("Decor.css");
+    }
+    
+        public void Action(Integer ACTION){
+            switch(ACTION){
+                case 0:
+                    System.out.println("ACTION A");
+                    break;
+                case 1:
+                    System.out.println("ACTION B");
+                    break;
+                case 2:
+                    System.out.println("ACTION C");
+                    break;
+                case 3:
+                    System.out.println("ACTION D");
+                    break;
+                case 4:
+                    System.out.println("ACTION E");
+                    break;    
+                
+            }
+        }
 //testing changes
 //        changes
-    }
     
     public Scene getScreen(){
         return ENTRANCE;
