f031e659456d94b7a5cff9080b56e87ac9a07fb9
oscarneiva <oscarneivaeneto@gmail.com>
2021-09-29 11:22:36 -0300

    No seats anymore. Refactoring starting point.

--- src/Scenes/SeatMap.java
+++ src/Scenes/SeatMap.java
@@ -32,7 +32,7 @@ public class SeatMap {
     BackgroundImage BG;
     Scene ENTRANCE;
     BorderPane PANE;
-    HBox MENUBAR1,BOX1,BOX11,BOX2,BOX3,HBOX1,INPLANE;
+    HBox MENUBAR1,BOX1,BOX11,BOX2,BOX3,INPLANE;
     HBox[] SEATMAP1,SEATMAP2,SEATMAP3;
     VBox CONTAINER,VBOX1,VBOX2,VBOX3;
     MyAccount MYACCOUNT;
@@ -57,6 +57,7 @@ public class SeatMap {
         LINEFLIGHT = new LineFlight();
         LINES = new FlightController();        
         PROP = new Decorum();
+        //first hbox
         BOX1 = new HBox();
         
         FLIGHTNUM = new Text();
@@ -83,7 +84,6 @@ public class SeatMap {
             BOBOX[k].setHeight(20);
             BOBOX[k].setWidth(20);              
         } 
-        
         RECT = new Region();
         RECT.setStyle("-fx-background-color: red; -fx-border-style: solid; -fx-border-width: 1; -fx-border-color: black; -fx-min-width: 20; -fx-min-height:20; -fx-max-width:20; -fx-max-height: 20;");        
         BOBOX[0].setFill(Color.AQUA);
@@ -98,122 +98,13 @@ public class SeatMap {
         BOX2.setAlignment(Pos.TOP_CENTER);
         BOX2.setPadding(new Insets(10,0,10,0));
         BOX2.getChildren().addAll(BUSINESS,ECONOMYPLUS,ECONOMY,OCCUPIED);
-       
-        //seat map
-        HBOX1 = new HBox();
         
-        //seatmap
-        SEATMAP1 = new HBox[5];
-        SEATS1 = new Rectangle[5][5];
-        ALLOCATION1 = new Integer[5][5];
-        VBOX1 = new VBox(8);
-        
-        for (int k = 0; k<5; k++){
-            SEATMAP1[k] = new HBox(8);
-            for(int j = 0;j<4;j++){
-                SEATS1[k][j] = new Rectangle();
-                SEATS1[k][j].setHeight(20);
-                SEATS1[k][j].setWidth(20);
-
-                if(k==1 & j==1){
-                    ALLOCATION1[k][j] = 2;                    
-                    SEATS1[k][j].setFill(Color.RED);                     
-                }
-                else{
-                    ALLOCATION1[k][j] = 0;      
-                    SEATS1[k][j].setFill(Color.AQUA);                     
-                }
-                final int INDEX = k;
-                final int INDEX2 = j;
-                SEATS1[INDEX][INDEX2].setOnMouseClicked((MouseEvent e) -> {
-                    if (ALLOCATION1[INDEX][INDEX2]!=2){
-                        if (ALLOCATION1[INDEX][INDEX2]!=1){
-                            SEATS1[INDEX][INDEX2].setFill(Color.BLACK);  
-                            ALLOCATION1[INDEX][INDEX2]=1;
-                        }else{
-                            SEATS1[INDEX][INDEX2].setFill(Color.AQUA);  
-                            ALLOCATION1[INDEX][INDEX2]=0;  
-                        }
-                    }
-                });
-                SEATMAP1[k].getChildren().addAll(SEATS1[k][j]);
-            }    
-            VBOX1.getChildren().addAll(SEATMAP1[k]);            
-        }
-        
-        SEATMAP2 = new HBox[5];
-        SEATS2 = new Rectangle[5][9];
-        ALLOCATION2 = new Integer[5][9];    
-        VBOX2 = new VBox(8);
-        
-        for (int k = 0;k<5;k++){
-            SEATMAP2[k] = new HBox(8);
-            for(int j = 0;j<8;j++){
-                SEATS2[k][j] = new Rectangle();
-                SEATS2[k][j].setHeight(20);
-                SEATS2[k][j].setWidth(20);
+        // Seat map
+        VBOX1 = new VBox();
                 
-                if((k==1 & j==1)||(k==2 & j==1)){
-                    ALLOCATION2[k][j] = 2;                    
-                    SEATS2[k][j].setFill(Color.RED);                     
-                }
-                else{
-                    ALLOCATION2[k][j] = 0;      
-                    SEATS2[k][j].setFill(Color.BURLYWOOD);                     
-                }                
-                final int INDEX = k;
-                final int INDEX2 = j;
-                SEATS2[k][j].setOnMouseClicked((MouseEvent e) -> {
-                    if (ALLOCATION2[INDEX][INDEX2]!=2){
-                        if (ALLOCATION2[INDEX][INDEX2]!=1){
-                            SEATS2[INDEX][INDEX2].setFill(Color.BLACK);  
-                            ALLOCATION2[INDEX][INDEX2]=1;
-                        }else{
-                            SEATS2[INDEX][INDEX2].setFill(Color.BURLYWOOD);  
-                            ALLOCATION2[INDEX][INDEX2]=0;                        
-                        }
-                    }
-                });
-                SEATMAP2[k].getChildren().addAll(SEATS2[k][j]);
-            }    
-            VBOX2.getChildren().addAll(SEATMAP2[k]);            
-        }
-        SUM2=0;
-        for (int k = 0;k<5;k++){       
-            for(int j = 0;j<8;j++){
-                if(ALLOCATION2[k][j]==1){
-                    SUM2 = SUM2+1;
-                }
-            }   
-        }    
-        SEATMAP3 = new HBox[5];
-        SEATS3 = new Rectangle[5][11];
-        ALLOCATION3 = new Integer[5][11];         
-        VBOX3 = new VBox(8);
-        
-        for (int k = 0;k<5;k++){
-            SEATMAP3[k] = new HBox(8);
-            for(int j = 0;j<10;j++){
-                SEATS3[k][j] = new Rectangle();
-                SEATS3[k][j].setHeight(20);
-                SEATS3[k][j].setWidth(20);
-                ALLOCATION3[k][j] = 0;  
-                SEATS3[k][j].setFill(Color.GOLD);
-                final int INDEX = k;
-                final int INDEX2 = j;
-                SEATS3[k][j].setOnMouseClicked((MouseEvent e) -> {
-                    if (ALLOCATION3[INDEX][INDEX2]!=1){
-                        SEATS3[INDEX][INDEX2].setFill(Color.BLACK);  
-                        ALLOCATION3[INDEX][INDEX2]=1;
-                    }else{
-                        SEATS3[INDEX][INDEX2].setFill(Color.GOLD);  
-                        ALLOCATION3[INDEX][INDEX2]=0;                        
-                    }
-                });
-                SEATMAP3[k].getChildren().addAll(SEATS3[k][j]);
-            }    
-            VBOX3.getChildren().addAll(SEATMAP3[k]);            
-        }        
+        VBOX2 = new VBox();
+                
+        VBOX3 = new VBox();
         
         BOX3 = new HBox();
         
@@ -269,13 +160,15 @@ public class SeatMap {
         BOX3.getChildren().add(CONTINUE);
         
         //main container
-        INPLANE = new HBox(20);
         BG= new BackgroundImage(new Image("Plane.png",1000,300,false,true),
             BackgroundRepeat.NO_REPEAT, 
             BackgroundRepeat.NO_REPEAT, 
             BackgroundPosition.CENTER,
             BackgroundSize.DEFAULT);
-//then you set to your node
+        
+        
+        // Boxes to distribute the seat map
+        INPLANE = new HBox(20);
         INPLANE.setBackground(new Background(BG));        
         INPLANE.getChildren().addAll(VBOX1,VBOX2,VBOX3);
         INPLANE.setAlignment(Pos.CENTER);
